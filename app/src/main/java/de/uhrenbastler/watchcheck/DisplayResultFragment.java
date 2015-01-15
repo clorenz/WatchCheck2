5a47b6f34a88afa460d57f4e58176fb845ba1c81
Christoph Lorenz <clorenz71@googlemail.com>
2015-01-15 17:53:28 +0100

    Initial work for manipulating result lsit

--- app/src/main/java/de/uhrenbastler/watchcheck/DisplayResultFragment.java
+++ app/src/main/java/de/uhrenbastler/watchcheck/DisplayResultFragment.java
@@ -2,10 +2,15 @@ package de.uhrenbastler.watchcheck;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.service.media.MediaBrowserService;
 import android.support.v4.app.Fragment;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -13,12 +18,14 @@ import android.widget.ListView;
 import com.shamanland.fab.FloatingActionButton;
 import com.shamanland.fab.ShowHideOnScroll;
 
+import java.util.Date;
 import java.util.List;
 
 import de.uhrenbastler.watchcheck.managers.ResultManager;
 import de.uhrenbastler.watchcheck.tools.Logger;
 import de.uhrenbastler.watchcheck.views.ResultListAdapter;
 import watchcheck.db.Log;
+import watchcheck.db.LogDao;
 import watchcheck.db.Watch;
 import watchcheck.db.WatchDao;
 
@@ -29,7 +36,7 @@ public class DisplayResultFragment extends Fragment {
 
     // Store instance variables
     private Watch currentWatch;
-    private List<Log> log;
+    private List<Log> log=null;
     private Log lastLog;
     private long watchId;
     private int period;
@@ -79,6 +86,7 @@ public class DisplayResultFragment extends Fragment {
         listView = (ListView) view.findViewById(R.id.resultListView);
         resultListAdapter = new ResultListAdapter(this.getActivity().getApplicationContext(), log);
         listView.setAdapter(resultListAdapter);
+        registerForContextMenu(listView);
 
         FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.buttonAddLog);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -92,8 +100,55 @@ public class DisplayResultFragment extends Fragment {
         });
         listView.setOnTouchListener(new ShowHideOnScroll(fab));
 
+        /*
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Log log = (Log) parent.getItemAtPosition(position);
+                Logger.debug("Position="+position+", id="+id+", item="+log.getId());
+                return false;
+            }
+        });
+        */
+
 
         return view;
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        if ( v.getId() == R.id.resultListView) {
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+            Log logToHandle = (Log)listView.getAdapter().getItem(info.position);
+            info.id=logToHandle.getId();            // Yuck!
+            menu.setHeaderTitle("Log from "+logToHandle.getReferenceTime());
+            String[] menuItems = getResources().getStringArray(R.array.resultlist_contextmenu);
+            for (int i = 0; i<menuItems.length; i++) {
+                menu.add(Menu.NONE, i, i, menuItems[i]);
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        final AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        int menuItemIndex = item.getItemId();
+
+        LogDao logDao = ((WatchCheckApplication)getActivity().getApplicationContext()).getDaoSession().getLogDao();
+        Log logToHandle = logDao.load(info.id);
+
+
+        switch ( menuItemIndex ) {
+            case 0: Logger.debug("Edit item "+logToHandle.getReferenceTime());
+                break;
+            case 1: Logger.debug("Delete item "+logToHandle.getReferenceTime());
+                break;
+        }
+
+
+
+        return true;
+    }
 }
