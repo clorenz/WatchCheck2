6fc5d97bc0984a4eb53a94b11c25ce54d989f027
Christoph Lorenz <clorenz71@googlemail.com>
2015-03-26 10:35:37 +0100

    Set intent after reboot, too

--- app/src/main/java/de/uhrenbastler/watchcheck/reminder/ReminderReceiver.java
+++ app/src/main/java/de/uhrenbastler/watchcheck/reminder/ReminderReceiver.java
@@ -8,21 +8,47 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.preference.PreferenceManager;
 import android.support.v4.app.NotificationCompat;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
 
 import de.uhrenbastler.watchcheck.MainActivity;
 import de.uhrenbastler.watchcheck.R;
+import de.uhrenbastler.watchcheck.tools.Logger;
 
 /**
  * Created by clorenz on 23.03.15.
  */
 public class ReminderReceiver extends BroadcastReceiver {
 
+    private static SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        showNotification(context);
+        Logger.info("Received reminder intent "+intent.getAction());
+        if ( intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            rescheduleAlarm(context);
+        } else {
+            showNotification(context);
+        }
+    }
+
+    private void rescheduleAlarm(Context context) {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean reminderPreferenceExists = sharedPref.contains("pref_reminder");
+        boolean alarmEnabled = sharedPref.getBoolean("pref_reminder", false);
+        long alarmSchedule = sharedPref.getLong("pref_reminder_time",0);
+
+        if ( reminderPreferenceExists && alarmEnabled ) {
+            ReminderManager.setAlarm(context, alarmSchedule, false, false);
+            String timeString = sdf.format(alarmSchedule);
+            Logger.info("Received boot completed intent; scheduling reminder to "+timeString);
+        }
     }
 
     private void showNotification(Context context) {
